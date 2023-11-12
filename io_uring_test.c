d2ec9fb6bc57b3afbc67632b899cfd8a606d13e8
arpit2607 <61446590+arpit2607@users.noreply.github.com>
2023-11-12 13:16:38 -0700

    Update io_uring_test.c

--- io_uring_test.c
+++ io_uring_test.c
@@ -10,12 +10,78 @@
 #define FILE_PATH "testfile"
 #define BUFFER_SIZE 1024
 
+void perform_io_uring_write(int fd, struct io_uring *ring, char *buffer, unsigned nbytes) {
+    struct io_uring_sqe *sqe;
+    struct io_uring_cqe *cqe;
+    int ret;
+
+    sqe = io_uring_get_sqe(ring);
+    if (!sqe) {
+        fprintf(stderr, "Could not get SQE for write.\n");
+        return;
+    }
+
+    io_uring_prep_write(sqe, fd, buffer, nbytes, 0);
+
+    ret = io_uring_submit(ring);
+    if (ret < 0) {
+        fprintf(stderr, "io_uring_submit for write: %s\n", strerror(-ret));
+        return;
+    }
+
+    ret = io_uring_wait_cqe(ring, &cqe);
+    if (ret < 0) {
+        fprintf(stderr, "io_uring_wait_cqe for write: %s\n", strerror(-ret));
+        return;
+    }
+
+    if (cqe->res < 0) {
+        fprintf(stderr, "Async write failed: %s\n", strerror(-cqe->res));
+    } else {
+        printf("Wrote %d bytes.\n", cqe->res);
+    }
+
+    io_uring_cqe_seen(ring, cqe);
+}
+
+void perform_io_uring_read(int fd, struct io_uring *ring, char *buffer, unsigned nbytes) {
+    struct io_uring_sqe *sqe;
+    struct io_uring_cqe *cqe;
+    int ret;
+
+    sqe = io_uring_get_sqe(ring);
+    if (!sqe) {
+        fprintf(stderr, "Could not get SQE for read.\n");
+        return;
+    }
+
+    io_uring_prep_read(sqe, fd, buffer, nbytes, 0);
+
+    ret = io_uring_submit(ring);
+    if (ret < 0) {
+        fprintf(stderr, "io_uring_submit for read: %s\n", strerror(-ret));
+        return;
+    }
+
+    ret = io_uring_wait_cqe(ring, &cqe);
+    if (ret < 0) {
+        fprintf(stderr, "io_uring_wait_cqe for read: %s\n", strerror(-ret));
+        return;
+    }
+
+    if (cqe->res < 0) {
+        fprintf(stderr, "Async read failed: %s\n", strerror(-cqe->res));
+    } else {
+        printf("Read %d bytes: %.*s\n", cqe->res, cqe->res, buffer);
+    }
+
+    io_uring_cqe_seen(ring, cqe);
+}
+
 void perform_io_uring_operation(int sqpoll) {
     struct io_uring ring;
     struct io_uring_params params;
     int fd, ret;
-    struct io_uring_sqe *sqe;
-    struct io_uring_cqe *cqe;
     char buffer[BUFFER_SIZE] = "Hello, io_uring!";
 
     if (sqpoll) {
@@ -33,39 +99,21 @@ void perform_io_uring_operation(int sqpoll) {
         return;
     }
 
-    fd = open(FILE_PATH, O_WRONLY | O_CREAT, 0644);
+    // Open file for read and write
+    fd = open(FILE_PATH, O_RDWR | O_CREAT, 0644);
     if (fd < 0) {
         perror("open");
         return;
     }
 
-    sqe = io_uring_get_sqe(&ring);
-    if (!sqe) {
-        fprintf(stderr, "Could not get SQE.\n");
-        return;
-    }
+    // Perform write operation
+    perform_io_uring_write(fd, &ring, buffer, BUFFER_SIZE);
 
-    io_uring_prep_write(sqe, fd, buffer, BUFFER_SIZE, 0);
+    // Reset file offset for reading
+    lseek(fd, 0, SEEK_SET);
 
-    ret = io_uring_submit(&ring);
-    if (ret < 0) {
-        fprintf(stderr, "io_uring_submit: %s\n", strerror(-ret));
-        return;
-    }
-
-    ret = io_uring_wait_cqe(&ring, &cqe);
-    if (ret < 0) {
-        fprintf(stderr, "io_uring_wait_cqe: %s\n", strerror(-ret));
-        return;
-    }
-
-    if (cqe->res < 0) {
-        fprintf(stderr, "Async write failed: %s\n", strerror(-cqe->res));
-    } else {
-        printf("Wrote %d bytes.\n", cqe->res);
-    }
-
-    io_uring_cqe_seen(&ring, cqe);
+    // Perform read operation
+    perform_io_uring_read(fd, &ring, buffer, BUFFER_SIZE);
 
     close(fd);
     io_uring_queue_exit(&ring);
